475027c2c9a0eef4b6d9d4847b7d3152b54db7e2
Gene Boggs <gene@ology.net>
2025-01-18 13:35:10 -0800

    No

--- junkdrawer.pl
+++ junkdrawer.pl
@@ -178,7 +178,7 @@ __DATA__
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Save as...</h5>
+        <h5 class="modal-title">Save...</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
