8c42d68fb9f110b058ff1518e48d7076614c215d
Gene Boggs <gene@ology.net>
2025-01-22 19:16:28 -0800

    Path

--- junkdrawer.pl
+++ junkdrawer.pl
@@ -215,12 +215,15 @@ post '/new_folder' => sub ($c) {
 
 sub _dir_iter {
   my ($c, $where, $children) = @_;
+  my $user = $c->session->{user};
+  my $backup = path(BACKUP);
   my $nf = Number::Format->new;
   my $iter = $where->iterator({ follow_symlinks => 1 });
   while (my $path = $iter->()) {
+    (my $place = $path) =~ s/$backup\/$user\///;
     my $stat = $path->stat;
     push @$children, {
-      name   => $path->basename,
+      name   => $place,
       path   => $path,
       size   => $nf->format_bytes($stat->[7]),
       bytes  => $stat->[7],
